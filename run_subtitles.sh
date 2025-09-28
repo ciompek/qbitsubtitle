2b810649a9eadaf9b3ed3aca688afbc578a087a3
ciompek <ciompek@gmail.com>
2025-09-28 18:04:51 +0200

    Create run_subtitles.sh

---
+++ run_subtitles.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# run_subtitles.sh
+# Called by qBittorrent after a torrent finishes downloading
+
+FOLDER="$1"
+
+if [ -z "$FOLDER" ]; then
+    echo "❌ No folder specified"
+    exit 1
+fi
+
+# Run the Python script
+python3 /opt/subtitles/download_subtitles.py "$FOLDER" --debug
